aaaa9c8ce93becedbb9f35b39a024cfd7587465e
Gabriel Silva <gabrielnsilva1@hotmail.com>
2019-11-27 22:28:35 -0300

    Mecanicas do  jogo | Arquivos dos artistas

---
+++ paperballRV/Assets/Scripts/Ball.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Ball : MonoBehaviour
+{
+
+    public float power = 150f;
+    public float powerincrease = 0.4f;
+    private bool moving = false;
+    public Collider coll;
+    Vector3 initialPos;
+    Transform pai;
+    Score score;
+    public TextMeshProUGUI powertxt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Ball Start");
+        score = (Score) FindObjectOfType(typeof(Score));
+
+        pai = transform.parent;
+        initialPos = transform.localPosition;
+
+        coll = GetComponent<Collider>();
+        coll.attachedRigidbody.useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (score.in_game){
+            if (Input.GetButton("Fire1") && !moving)
+            {
+                power += powerincrease;
+                powertxt.text = "Power: "+Mathf.RoundToInt(power/10).ToString();
+                if (power > 300 || power < 0){
+                    powerincrease = powerincrease*(-1);
+                }
+            }
+            if (Input.GetButtonDown("Fire1") && !moving)
+            {
+                power = 150;
+                powertxt.gameObject.SetActive(true);
+            }
+            if (Input.GetButtonUp("Fire1") && !moving)
+            {
+                moving = true;
+                powertxt.gameObject.SetActive(false);
+                coll.attachedRigidbody.useGravity = true;
+                this.Launch(transform.forward, power);
+            }
+        } else {
+            if (Input.GetButtonUp("Fire1"))
+            {
+                score.restart();
+                Start();
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        print("Colisao");
+
+        if(collision.gameObject.name == "target"){
+            print("Ponto");
+            score.hit();
+            // Destroy(gameObject);
+        } else {
+            print("Perdida");
+            score.missed();
+            // power = 0;
+        }
+
+        GetComponent<Rigidbody>().velocity = Vector3.zero;
+        moving = false;
+        coll.attachedRigidbody.useGravity = false;
+
+        transform.parent = pai;
+        transform.localPosition = initialPos;
+    }
+
+    void Launch(Vector3 direction, float power)
+    {
+        //transform.position += direction * power;
+        transform.parent = null;
+
+        GetComponent<Rigidbody>().AddForce(direction * power);
+    }
+}
---
+++ paperballRV/Assets/Scripts/Score.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Score : MonoBehaviour
+{
+
+    public bool in_game = true;
+    private int score = 0;
+    public int attempts = 15;
+    private int initial_attempts;
+    private int initial_stress;
+    private int stress = 100;
+    public TextMeshProUGUI scoretxt;
+    public TextMeshProUGUI ballstxt;
+    public TextMeshProUGUI stresstxt;
+    public TextMeshProUGUI hitstxt;
+    public TextMeshProUGUI endtxt;
+    public TextMeshProUGUI finalmessagetxt;
+    public Vector3[] posicoes;
+    public Transform trashcan;
+    // Start is called before the first frame update
+    void Start()
+    {
+        in_game = true;
+        score = 0;
+        initial_attempts = attempts;
+        stress = Random.Range(80, 80);
+        initial_stress = stress;
+        trashcan.position = posicoes[Random.Range(0, posicoes.Length)];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (attempts <= 0 || stress >= 100){
+            scoretxt.gameObject.SetActive(true);
+            endtxt.gameObject.SetActive(true);
+            finalmessagetxt.gameObject.SetActive(true);
+            scoretxt.text = "Score: "+score;
+            if (stress <= 50){
+                endtxt.text = "WIN!";
+                finalmessagetxt.text = "Congratz! Your stress decreased "+
+                                        (initial_stress-stress)+
+                                        "%\nPress \"Fire1\" to restart game";
+            } else {
+                endtxt.text = "Game Over!";
+                finalmessagetxt.text = "Your stress decreased "+
+                                        (initial_stress-stress)+"%\n"+
+                                        "but it's still over 50%\n"+
+                                        "Press \"Fire1\" to restart game";
+                if (stress >= 100)
+                    finalmessagetxt.text = "Your stress has reached 100%\n"+
+                                           "You had a heart attack and died!\n"+
+                                           "Press \"Fire1\" to restart game";
+            }
+            in_game = false;
+        } else {
+            scoretxt.gameObject.SetActive(false);
+            endtxt.gameObject.SetActive(false);
+            finalmessagetxt.gameObject.SetActive(false);
+        }
+
+        ballstxt.text = "Remaining Balls: " + attempts;
+        stresstxt.text = "Stress: "+stress+"%";
+        hitstxt.text = "Hits: "+score+"/"+(initial_attempts-attempts);
+    }
+
+    public void hit(){
+        score += 1;
+        attempts -= 1;
+        stress -= 5;
+        trashcan.position = posicoes[Random.Range(0, posicoes.Length)];
+    }
+
+    public void missed(){
+        attempts -= 1;
+        stress += 2;
+    }
+
+    public void restart(){
+        attempts = initial_attempts;
+        Start();
+    }
+}
